97e3c08e67bfb708673ab6c1f020740abb130e84
larry <larz2050@gmail.com>
2014-11-23 00:02:30 -0500

    Added fail tests and comments

--- test/ProgramFiles/articles/ArticlesDAOTest.java
+++ test/ProgramFiles/articles/ArticlesDAOTest.java
@@ -9,40 +9,67 @@ import static org.junit.Assert.*;
 /**
  *
  * @author Larry Morales
+ * Integration tests for article CRUD operations
  * 
  */
 public class ArticlesDAOTest {
     
+    
+    /**
+     * Tests whether all available articles are retrieved
+     */
     @Test
     public void testGetAllArticles() {
         List<ArticleBean> articleList = ArticlesDAO.getAllArticles();
         assertTrue(articleList.size() > 0);
     }
     
+    /**
+     * Tests whether all articles from given user are retrieved
+     */
     @Test
     public void testGetArticlesByUser() {
-        List<ArticleBean> articleList = ArticlesDAO.getArticlesByUser("10");
+        String User_ID = "10";
+        List<ArticleBean> articleList = ArticlesDAO.getArticlesByUser(User_ID);
         assertTrue(articleList.size() > 0);
     }
     
+    /**
+     * Tests whether the article with the given ID is retrieved
+     */
     @Test
     public void testGetArticle() {
-        ArticleBean article = ArticlesDAO.getArticle(4);
+        int Article_ID = 4;
+        ArticleBean article = ArticlesDAO.getArticle(Article_ID);
         assertNotNull(article);
         assertTrue(article.getArticle_Id() == 4);
     }
     
+    /**
+     * Tests whether the article with the given invalid ID is retrieved (fail test)
+     */
+    @Test
+    public void testGetArticleFail() {
+        int Article_ID = -1;
+        ArticleBean article = ArticlesDAO.getArticle(Article_ID);
+        assertNull(article);
+    }
+    
+    /**
+     * Tests whether new article can be created
+     */
     @Test
     public void testCreateArticle() {
         ArticleBean article = new ArticleBean();
+        String User_ID = "10";
         
         UserBean user = new UserBean();
-        user.setUser_ID("10");
+        user.setUser_ID(User_ID);
         article.setUser(user);
         
         article.setArticle_Date(new Date(System.currentTimeMillis()));
         article.setArticle_Title("Create Test");
-        article.setArticle_Description("JUnite Create Test");
+        article.setArticle_Description("JUnit Create Test");
         article.setArticle_Location("http://newsfoil.com");
         article.setArticle_Rank(5);
         
@@ -50,12 +77,38 @@ public class ArticlesDAOTest {
         assertTrue(success);
     }
     
+    /**
+     * Tests whether article with invalid user can be created (fail test)
+     */
+    @Test
+    public void testCreateInvalidArticle() {
+        ArticleBean article = new ArticleBean();
+        String User_ID = "-1";
+        
+        UserBean user = new UserBean();
+        user.setUser_ID(User_ID);
+        article.setUser(user);
+        
+        article.setArticle_Date(new Date(System.currentTimeMillis()));
+        article.setArticle_Title("Create Fail Test");
+        article.setArticle_Description("Create Fail Test");
+        article.setArticle_Location("http://newsfoil.com");
+        article.setArticle_Rank(5);
+        
+        boolean success = ArticlesDAO.createArticle(article);
+        assertFalse(success);
+    }
+    
+    /**
+     * Tests whether existing article can be updated
+     */
     @Test
     public void testUpdateArticle() {
         ArticleBean article = new ArticleBean();
+        String User_ID = "10";
         
         UserBean user = new UserBean();
-        user.setUser_ID("10");
+        user.setUser_ID(User_ID);
         article.setUser(user);
         
         article.setArticle_Id(4);
@@ -65,8 +118,31 @@ public class ArticlesDAOTest {
         article.setArticle_Location("http://newsfoil.com");
         article.setArticle_Rank(5);
         
-        boolean success = ArticlesDAO.createArticle(article);
+        boolean success = ArticlesDAO.updateArticle(article);
         assertTrue(success);
+    }
+    
+     /**
+     * Tests whether article with invalid ID can be updated (fail test)
+     */
+    @Test
+    public void testUpdateInvalidArticle() {
+        ArticleBean article = new ArticleBean();
+        String User_ID = "-1";
+        
+        UserBean user = new UserBean();
+        user.setUser_ID(User_ID);
+        article.setUser(user);
+        
+        article.setArticle_Id(-1);
+        article.setArticle_Date(new Date(System.currentTimeMillis()));
+        article.setArticle_Title("Update Fail Test");
+        article.setArticle_Description("Update Faile Test");
+        article.setArticle_Location("http://newsfoil.com");
+        article.setArticle_Rank(5);
+        
+        boolean success = ArticlesDAO.updateArticle(article);
+        assertFalse(success);
     }
     
 //    @Test
