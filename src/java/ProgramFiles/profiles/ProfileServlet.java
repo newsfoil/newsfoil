d2e3a0dc0ada724779330a1057327039f306fbf4
larry <larry.r.morales@outlook.com>
2014-11-24 23:18:00 -0500

    Initial commit

--- src/java/ProgramFiles/profiles/ProfileServlet.java
+++ src/java/ProgramFiles/profiles/ProfileServlet.java
@@ -5,10 +5,33 @@
  */
 package ProgramFiles.profiles;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  *
  * @author larry
  */
-public class ProfileServlet {
+@WebServlet("/ProfilePage")
+public class ProfileServlet extends HttpServlet {
     
+        /**
+     * Returns ProfilePage.jsp with the information it needs to render.
+     * @param request The browser request
+     * @param response The browser response
+     * @throws ServletException
+     * @throws IOException 
+     */
+    @Override
+    public void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        request.getRequestDispatcher("ProfilePage.jsp").forward(request, response);
+    }
 }
